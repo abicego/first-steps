85c144aec326ca60315ae9228fd408a14d6730fe
Angélica Bicego <59181068+abicego@users.noreply.github.com>
2021-09-11 17:59:58 -0300

    Update gradeAprovation.java

--- gradeAprovation.java
+++ gradeAprovation.java
@@ -10,15 +10,15 @@ public class gradeAprovation {
         percentualFalta = Integer.parseInt(JOptionPane.showInputDialog("Digite o percentual de faltas de aluno: "));
         
         if 	((notaAluno >=70 ) && (percentualFalta <=25)){
-            System.out.println("Passou");
+            System.out.println("Parabéns, você passou!");
 
         } else{
             
             if ((notaAluno >=40) && (percentualFalta <=25)) {
-                System.out.println("Prova Alternativa");
+                System.out.println("Você tem a oportunidade de fazer uma prova alternativa!");
             }
             else {
-                System.out.println("Não passou");
+                System.out.println("Infelizmente, você não passou!");
             }
         }
     }
